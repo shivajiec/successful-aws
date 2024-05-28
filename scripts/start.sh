1e4d0a3cc080837da352f0f211c4c331a201484d
shiva <shiva@gmail.com>
2024-05-28 13:01:27 +0530

    EOF error

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,5 +5,5 @@ set -e
 echo "pulling docker image from docker hub"
 docker pull shivadmin/adi-janu:latest
 
-echo "creating docker container
+echo "creating docker container"
 docker run -d -p 5000:5000 shivadmin/adi-janu:latest
